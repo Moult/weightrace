44d13454b8e76defc3ee20ade82b3be3dfb4a1e5
Dion Moult <dion@thinkmoult.com>
2013-07-17 15:04:53 +1000

    Give dummy data for mustache loops

--- application/classes/View/Static/Homepage.php
+++ application/classes/View/Static/Homepage.php
@@ -4,4 +4,39 @@ defined('SYSPATH') OR die('No direct script access.');
 /**
  * Shows site index / homepage.
  */
-class View_Static_Homepage extends View_Layout {}
+class View_Static_Homepage extends View_Layout
+{
+    public function racers()
+    {
+        return array(
+            array(
+                'n' => 1,
+                'tagline' => 'The proud initiator'
+            ),
+            array(
+                'n' => 2,
+                'tagline' => 'The daring nemesis'
+            ),
+            array(
+                'n' => 3,
+                'tagline' => 'The crafty third-party'
+            ),
+            array(
+                'n' => 4,
+                'tagline' => 'The stealthy underdog'
+            )
+        );
+    }
+
+    public function losers()
+    {
+        return array(
+            array(
+                'name' => 'Walter Biggins'
+            ),
+            array(
+                'name' => 'Walter Biggins'
+            )
+        );
+    }
+}
