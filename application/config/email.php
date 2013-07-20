9e77fdd2734ce4da132130cb32c1653ed9cd930e
Dion Moult <dion@thinkmoult.com>
2013-07-20 18:51:03 +1000

    Add config files

---
+++ application/config/email.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+    'smtp' => array(
+        'host' => 'mail.website.com',
+        'port' => 465,
+        'username' => 'username',
+        'password' => 'password'
+    )
+);
