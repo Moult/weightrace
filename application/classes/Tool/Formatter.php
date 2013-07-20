535783cab938c4f92d4abdf24b7eadfd43334191
Dion Moult <dion@thinkmoult.com>
2013-07-20 18:41:45 +1000

    Implement emailer and formatter

---
+++ application/classes/Tool/Formatter.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+class Tool_Formatter extends Driver_Formatter implements Welgam\Core\Tool\Formatter {}
---
+++ application/classes/Tool/Validator.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @license MIT
+ * Full license text in LICENSE file
+ */
+
+class Tool_Validator extends Driver_Validator implements Welgam\Core\Tool\Validator {}
