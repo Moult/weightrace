b91ec9677b1e10852a06269eee27c03f3255b35b
Dion Moult <dion@thinkmoult.com>
2013-08-02 23:28:53 +1000

    Make nag email nicer with health tips

--- application/classes/View/Email/Competition/Notify/Body.php
+++ application/classes/View/Email/Competition/Notify/Body.php
@@ -32,4 +32,22 @@ class View_Email_Competition_Notify_Body extends View_Layout
     {
         return $this->data['racer_password'];
     }
+
+    public function health_tip()
+    {
+        $tips = array(
+            'walk at least 30 minutes daily',
+            'eat fruit every day',
+            'being vegetarian but eating cheese all the time isn\'t healthier',
+            'drink plenty of water',
+            'eat slowly',
+            'know that healthy food tastes great later',
+            'chew food completely before swallowing',
+            'don\'t over eat',
+            'cut down on sugary foods',
+            'weight control is a mixture of lifestyle and diet'
+        );
+
+        return $tips[rand(0, count($tips) - 1)];
+    }
 }
